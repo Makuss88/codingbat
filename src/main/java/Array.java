184b2980c3234da8bca58db930d7b061cb8e8249
MakussBlue <g.maciborek@wp.pl>
2019-05-10 19:27:51 +0200

    next problem solving, "shiftLeft" / Array-2

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -247,4 +247,21 @@ public class Array {
     }
 
 
+    // Return an array that is "left shifted" by one -- so {6, 2, 5, 3}
+    // returns {2, 5, 3, 6}. You may modify and return the given array, or return a new array
+    public int[] shiftLeft(int[] nums) {
+
+        if (nums.length == 0) return nums;
+        int temp = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[(i + 1) % nums.length];
+        }
+
+        nums[nums.length - 1] = temp;
+
+        return nums;
+    }
+
+
 }
