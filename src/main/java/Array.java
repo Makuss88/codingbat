c73914a88fc8a6e9e1ece868b7fd88a077fdc8c8
MakussBlue <g.maciborek@wp.pl>
2019-05-13 23:24:22 +0200

    next problem solving, "notAlone"/ Array-2

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -346,7 +346,35 @@ public class Array {
 
     }
 
+    // We'll say that an element in an array is "alone" if there are values before and after it,
+    // and those values are different from it. Return a version of the given array where every instance
+    // of the given value which is alone is replaced by whichever value to its left or right is larger.
+    public int[] notAlone(int[] nums, int val) {
 
+        if (nums.length <= 2) {
+            return nums;
+        }
+
+        int[] result = new int[nums.length];
+
+        result[0] = nums[0];
+        result[nums.length - 1] = nums[nums.length - 1];
+
+        for (int i = 1; i < nums.length - 1; i++) {
+
+            if (nums[i] == val) {
+                if (nums[i - 1] > nums[i + 1]) {
+                    result[i] = nums[i - 1];
+                    continue;
+                } else {
+                    result[i] = nums[i + 1];
+                    continue;
+                }
+            }
+            result[i] = nums[i];
+        }
+        return result;
+    } // in my opi this option is good -  and website say - BAD!
 
 
 }
