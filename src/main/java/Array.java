f2d022ac8e2a04a9a5f56888a219245987dc02d0
MakussBlue <g.maciborek@wp.pl>
2019-05-14 14:03:22 +0200

    next problem solving, "withoutTeen"/ Array-2

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -404,4 +404,24 @@ public class Array {
         return result;
     }
 
+    // Return a version of the given array where all the 10's have been removed. The remaining elements should
+    // shift left towards the start of the array as needed, and the empty spaces a the end of the array should be 0.
+    // So {1, 10, 10, 2} yields {1, 2, 0, 0}. You may modify and return the given array or make a new array.
+    public int[] withoutTen(int[] nums) {
+
+        int result[] = new int[nums.length];
+        int count = 0;
+
+        for (int num : nums) {
+            if (num % 10 != 0) {
+                result[count] = num;
+                count++;
+            }
+        }
+        return result;
+
+    }
+
+
+
 }
