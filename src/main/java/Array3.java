3e0999a4d3c5bc7efaab535c4291fa51ab31edd9
MakussBlue <g.maciborek@wp.pl>
2019-05-24 22:39:03 +0200

    next problem solving, "maxSpan2"  - in one loop - it's faster / Array-3

--- src/main/java/Array3.java
+++ src/main/java/Array3.java
@@ -21,10 +21,40 @@ public class Array3 {
         }
 
         for (int i = nums.length - 1; i >= 0; i--) {
-            if (nums[i] == rightNumber){
-                rightCount = nums.length-i;
+            if (nums[i] == rightNumber) {
+                rightCount = nums.length - i;
             }
         }
+
+        if (leftCount > rightCount) {
+            return leftCount;
+        } else {
+            return rightCount;
+        }
+    }
+    // the same method, but in one loop - it's faster
+    public int maxSpan2(int[] nums) {
+
+        if (nums.length < 2) {
+            return nums.length;
+        }
+
+        int leftCount = 0;
+        int rightCount = 0;
+        int leftNumber = nums[0];
+        int rightNumber = nums[nums.length - 1];
+        boolean right = true;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == leftNumber) {
+                leftCount = i;
+            }
+            if (nums[i] == rightNumber && right) {
+                right = false;
+                rightCount = nums.length - i;
+            }
+        }
+
 
         if (leftCount > rightCount) {
             return leftCount;
