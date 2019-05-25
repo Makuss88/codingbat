ab143614efa502e3b1c2d635cc3968d48beb78b3
MakussBlue <g.maciborek@wp.pl>
2019-05-25 23:14:07 +0200

    next problem solving, "fix34" / Array-3,

--- src/main/java/Array3.java
+++ src/main/java/Array3.java
@@ -64,5 +64,29 @@ public class Array3 {
         }
     }
 
+    // Return an array that contains exactly the same numbers as the given array, but rearranged so that every 3 is
+    // immediately followed by a 4. Do not move the 3's, but every other number may move. The array contains the same
+    // number of 3's and 4's, every 3 has a number after it that is not a 3, and a 3 appears in the array before any 4.
+    public int[] fix34(int[] nums) {
+
+        int four = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 3) {
+
+                for (int j = four; j < nums.length; j++) {
+                    if (nums[j] == 4) {
+                        int temp = nums[i + 1];
+                        nums[i + 1] = 4;
+                        nums[j] = temp;
+                        four = j;
+                        break;
+                    }
+                }
+            }
+        }
+        return nums;
+    }
+
 
 }
