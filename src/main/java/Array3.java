615567756427ef17bd3128ae3114e159404af8fc
MakussBlue <g.maciborek@wp.pl>
2019-05-27 22:44:40 +0200

    next problem solved, "canBalance" / Array-3

--- src/main/java/Array3.java
+++ src/main/java/Array3.java
@@ -120,5 +120,28 @@ public class Array3 {
         return nums;
     }
 
+    // Given a non-empty array, return true if there is a place to split the array so that the sum of the numbers
+    // on one side is equal to the sum of the numbers on the other side.
+    public boolean canBalance(int[] nums) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            int left = 0;
+            int right = 0;
+
+            for (int j = 0; j < i + 1; j++) {
+                left += nums[j];
+            }
+
+            for (int j = i + 1; j < nums.length; j++) {
+                right += nums[j];
+            }
+
+            if (left == right) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
