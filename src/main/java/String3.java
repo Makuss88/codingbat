125de3c8e32975dd6405cf97b43a6329c76ec630
MakussBlue <g.maciborek@wp.pl>
2019-06-07 21:22:45 +0200

    next problem solved "gHappy" / String3

--- src/main/java/String3.java
+++ src/main/java/String3.java
@@ -61,4 +61,19 @@ public class String3 {
         return is == not;
     }
 
+
+    // We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or
+    // right. Return true if all the g's in the given string are happy.
+    public boolean gHappy(String str) {
+
+        str = "x" + str + "x";
+
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) == 'g' && str.charAt(i - 1) != 'g' && str.charAt(i + 1) != 'g') {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
