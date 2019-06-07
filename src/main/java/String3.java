f2fa32cd7dbd021699de0a12a00f1bfd59f345d1
MakussBlue <g.maciborek@wp.pl>
2019-06-07 21:05:03 +0200

    next problem solved "equalIsNot" / String3

--- src/main/java/String3.java
+++ src/main/java/String3.java
@@ -42,4 +42,23 @@ public class String3 {
     }
 
 
+    // Given a string, return true if the number of appearances of "is" anywhere in the string is equal to the
+    // number of appearances of "not" anywhere in the string (case sensitive).
+    public boolean equalIsNot(String str) {
+
+        int is = 0;
+        int not = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.substring(i).startsWith("is")) {
+                is++;
+                continue;
+            }
+            if (str.substring(i).startsWith("not")) {
+                not++;
+            }
+        }
+        return is == not;
+    }
+
 }
