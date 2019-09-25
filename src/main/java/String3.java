885fb165ae8c9d3af9f6fecb9851b4f468087c36
MakussBlue <g.maciborek@wp.pl>
2019-09-25 21:39:54 +0200

    next problem solved "sumDigits", "sameEnds" / String3

--- src/main/java/String3.java
+++ src/main/java/String3.java
@@ -79,13 +79,12 @@ public class String3 {
 
     // We'll say that a "triple" in a string is a char appearing three times in a row. Return the number of triples
     // in the given string. The triples may overlap.
-
     public int countTriple(String str) {
 
         int result = 0;
 
         for (int i = 0; i < str.length() - 2; i++) {
-            if (str.charAt(i) == str.charAt(i+1) && str.charAt(i) == str.charAt(i+2)){
+            if (str.charAt(i) == str.charAt(i + 1) && str.charAt(i) == str.charAt(i + 2)) {
                 result++;
             }
         }
@@ -93,4 +92,40 @@ public class String3 {
     }
 
 
+    // Given a string, return the sum of the digits 0-9 that appear in the string, ignoring all other characters.
+    // Return 0 if there are no digits in the string. (Note: Character.isDigit(char) tests if a char is one of
+    // the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
+    public int sumDigits(String str) {
+
+        int result = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                result += Integer.parseInt(String.valueOf(str.charAt(i)));
+            }
+        }
+        return result;
+
+    }
+
+
+    // Given a string, return the longest substring that appears at both the beginning and end of the string
+    // without overlapping. For example, sameEnds("abXab") is "ab".
+    public String sameEnds(String string) {
+
+        String result = "";
+        String temp = "";
+
+        for (int i = 0; i < string.length(); i++) {
+
+            temp += string.charAt(i);
+
+            int templen = temp.length();
+
+            if (i < string.length() / 2 && temp.equals(string.substring(string.length() - templen)))
+                result = temp;
+        }
+        return result;
+    }
 }
+
